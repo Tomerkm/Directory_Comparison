3bb81c74cfdb37bf2e0f607313807a04d9a8e4d3
Tomerkm <tomerkeiynan@gmail.com>
2024-01-18 16:36:40 +0200

    improved the code of cmp images

--- logic/compare_images_values.py
+++ logic/compare_images_values.py
@@ -2,7 +2,6 @@ from skimage.metrics import structural_similarity as ssim
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from PIL import Image, ImageChops
 from infra.utils import get_files, find_first_cond, get_file_name_from_path
 from logic.set_diff_and_common_names import common_names_two_folders
 
@@ -39,39 +38,48 @@ def mse(imageA, imageB):
 
 
 def compare_images(imageA, imageB, title):
+
+    ROW_SIZE = 4
+    COL_SIZE = 1
+
     # compute the mean squared error and structural similarity
     # index for the images
     m = mse(imageA, imageB)
     s = ssim(imageA, imageB)
+
+    diff_a_b = cv2.subtract(imageA, imageB)
+    diff_b_a = cv2.subtract(imageB, imageA)
 
     # setup the figure
     fig = plt.figure(title)
     plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
 
     # show first image
-    fig.add_subplot(1, 2, 1)
-    plt.imshow(imageA, cmap=plt.cm.gray)
+    fig.add_subplot(ROW_SIZE, COL_SIZE, 1)
+    plt.title("imageA")
+    plt.imshow(imageA)
     plt.axis("off")
 
     # show the second image
-    fig.add_subplot(1, 2, 2)
-    plt.imshow(imageB, cmap=plt.cm.gray)
+    fig.add_subplot(ROW_SIZE, COL_SIZE, 2)
+    plt.title("imageB")
+    plt.imshow(imageB)
+    plt.axis("off")
+
+    # show the third diff image
+    fig.add_subplot(ROW_SIZE, COL_SIZE, 3)
+    plt.title("imageA - ImageB")
+    plt.imshow(diff_a_b)
+    plt.axis("off")
+
+    # show the forth diff image
+    fig.add_subplot(ROW_SIZE, COL_SIZE, 4)
+    plt.title("imageB - ImageA")
+    plt.imshow(diff_b_a)
     plt.axis("off")
 
     # show the images
     plt.show()
-
-
-def compare_giff_images(imageA, imageB):
-    # assign images
-    img1 = Image.open(imageA)
-    img2 = Image.open(imageB)
-
-    # finding difference
-    diff = ImageChops.difference(img1, img2)
-
-    # showing the difference
-    diff.show()
 
 
 def compare_img_files(path_files_a: str, path_files_b: str, logger):
@@ -83,14 +91,10 @@ def compare_img_files(path_files_a: str, path_files_b: str, logger):
 
     for i in range(0, size):
         file_name = get_file_name_from_path(common_full_path_a[i])
-        if file_name.endswith(".jpg"):
-            image_val_a = cv2.imread(common_full_path_a[i])
-            image_val_b = cv2.imread(common_full_path_b[i])
+        image_val_a = cv2.imread(common_full_path_a[i])
+        image_val_b = cv2.imread(common_full_path_b[i])
 
-            image_val_a = cv2.cvtColor(image_val_a, cv2.COLOR_BGR2GRAY)
-            image_val_b = cv2.cvtColor(image_val_b, cv2.COLOR_BGR2GRAY)
+        image_val_a = cv2.cvtColor(image_val_a, cv2.COLOR_BGR2GRAY)
+        image_val_b = cv2.cvtColor(image_val_b, cv2.COLOR_BGR2GRAY)
 
-            compare_images(image_val_a, image_val_b, title=f"image name compare: {file_name}")
-
-        else:
-            compare_giff_images(common_full_path_a[i], common_full_path_b[i])
+        compare_images(image_val_a, image_val_b, title=f"image name compare: {file_name}")
