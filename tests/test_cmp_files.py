3ef59b592c5babaea94cce129c4ec8e5e29ea7e4
Tomerkm <tomerkeiynan@gmail.com>
2024-01-18 12:03:43 +0200

    check the list is not empty on each test

--- tests/test_cmp_files.py
+++ tests/test_cmp_files.py
@@ -8,12 +8,14 @@ from logic.compare_files_values import compare_file_txt_values, compare_file_pdf
                          ids=["compare text files"])
 def test_cmp_txt_files_values(path_files_a, path_files_b, get_test_name, get_logger, get_config):
     res = compare_file_txt_values(path_files_a, path_files_b, logger=get_logger)
+    assert len(res)
     for num in res:
         assert 0 < num
 
 
 def test_cmp_same_txt_files_values(get_test_name, get_logger, get_config):
     res = compare_file_txt_values(FOLDER_FILES_TYPE_TEXT_PATH, FOLDER_FILES_TYPE_TEXT_PATH, logger=get_logger)
+    assert len(res)
     for num in res:
         assert 0 == num
 
--- tests/test_file_names.py
+++ tests/test_file_names.py
@@ -9,6 +9,7 @@ from logic.set_diff_and_common_names import set_diff_names_two_folders, common_n
                          ids=["files text type in folder a but not in b", "files text type in folder b but not in a"])
 def test_cmp_txt_files_name(path_files_a, path_files_b, expected_res, get_test_name, get_logger, get_config):
     res = set_diff_names_two_folders(path_files_a, path_files_b, logger=get_logger)
+    assert len(res)
     for file_name in res:
         assert file_name in expected_res
 
@@ -17,6 +18,7 @@ def test_cmp_txt_files_name(path_files_a, path_files_b, expected_res, get_test_n
                          ids=["files pdf type in folder a but not in b", "files pdf type in folder b but not in a"])
 def test_cmp_pdf_files_name(path_files_a, path_files_b, expected_res, get_test_name, get_logger, get_config):
     res = set_diff_names_two_folders(path_files_a, path_files_b, logger=get_logger)
+    assert len(res)
     for file_name in res:
         assert file_name in expected_res
 
@@ -25,6 +27,7 @@ def test_cmp_pdf_files_name(path_files_a, path_files_b, expected_res, get_test_n
                          ids=["files in folder a but not in b", "files in folder b but not in a"])
 def test_cmp_diff_type_files_name(path_files_a, path_files_b, expected_res, get_test_name, get_logger, get_config):
     res = set_diff_names_two_folders(path_files_a, path_files_b, logger=get_logger)
+    assert len(res)
     for file_name in res:
         assert file_name in expected_res
 
@@ -33,6 +36,7 @@ def test_cmp_diff_type_files_name(path_files_a, path_files_b, expected_res, get_
                          ids=["common files names text type"])
 def test_common_names_type_text(path_files_a, path_files_b, expected_res, get_test_name, get_logger, get_config):
     res = common_names_two_folders(path_files_a, path_files_b, logger=get_logger)
+    assert len(res)
     for file_name in res:
         assert file_name in expected_res
 
@@ -41,5 +45,6 @@ def test_common_names_type_text(path_files_a, path_files_b, expected_res, get_te
                          ids=["common files names pdf type"])
 def test_common_names_type_pdf(path_files_a, path_files_b, expected_res, get_test_name, get_logger, get_config):
     res = common_names_two_folders(path_files_a, path_files_b, logger=get_logger)
+    assert len(res)
     for file_name in res:
         assert file_name in expected_res
